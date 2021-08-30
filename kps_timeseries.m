498ba73ed1b9bd4273d24ce1b3e8e229e02f4e4a
Elton Yeung <elton.yeung19@imperial.ac.uk>
2021-08-30 15:24:39 +0800

    fixed synchronize tt & added correlation analysis

--- kps_timeseries.m
+++ kps_timeseries.m
@@ -22,35 +22,41 @@ kps_phone{:,2:2:26} = -kps_phone{:,2:2:26};
 %% Time sync
 
 % select syncing keypoint
-var = 'right_wrist';
+var = 'left_wrist';
 % 
 % % ============ Time sync correction ==========
 phonesync = 3.7755;   % Enter time difference here  (Pilot5 = 3.7755; pilot6 = -0.9 )
 kps_phone.time = kps_phone.time + phonesync;
 % ============================================
 
-% Trim extra time
-% max_t = min([max(kps_kinect.time) max(kps_phone.time)]);
-% max_trounded = floor(max_t);
-% idx_k = regexp(string(kps_kinect.time), regexptranslate('wildcard', strcat(num2str(max_trounded),'.*')));
-% idx_p = regexp(string(kps_phone.time), regexptranslate('wildcard', strcat(num2str(max_trounded),'.*')));
-% idx_k = max(find(~cellfun(@isempty,idx_k)));
-% idx_p = max(find(~cellfun(@isempty,idx_p)));
-% kps_kinect(idx_k+1:length(kps_kinect.time),:) = [];
-% kps_phone(idx_p+1:length(kps_phone.time),:) = [];
-
 % Time sync with video
-t = -1;        %(pilot5: t= -4 ; pilot6: t = -1)
+t = -4;        %(pilot5: t= -4 ; pilot6: t = -1)
 kps_kinect.time = kps_kinect.time + t;
 kps_phone.time = kps_phone.time + t;
 
 % -- Print Figure
 figure('Name','(1) Time sync - Key point')
-scatter(kps_kinect.time, eval(strcat('kps_kinect.', var, '_y')),'M','x','MarkerEdgeColor',[0 .7 0],'LineWidth',1.5)
+scatter(kps_kinect.time, eval(strcat('kps_kinect.', var, '_y')),'M','x','MarkerEdgeColor',[0 .7 0])
 hold on
-scatter(kps_phone.time, eval(strcat('kps_phone.', var,'_y')))
+scatter(kps_phone.time, eval(strcat('kps_phone.', var,'_y')),'MarkerEdgeColor',[0 0 .7])
 
-clear idx_* max_t* t
+clear t var phonesync
+
+%% Trim extra time (Max & Min time)
+max_t = min([max(kps_kinect.time) max(kps_phone.time)]);
+idx1_k = kps_kinect.time >= max_t;
+idx1_p = kps_phone.time >= max_t;
+kps_kinect(idx1_k,:) = [];
+kps_phone(idx1_p,:) = [];
+
+min_t = max([min(kps_kinect.time) min(kps_phone.time)]);
+min_trounded = round(min_t,1);
+idx2_k = kps_kinect.time <= min_t;
+idx2_p = kps_phone.time <= min_t;
+kps_kinect(idx2_k,:) = [];
+kps_phone(idx2_p,:) = [];
+
+clear idx* min* max*
 
 %% Concat kinect & mediapipe timeseries into one timeseries cube
 
@@ -58,11 +64,13 @@ clear idx_* max_t* t
 ts_phone = table2timetable(kps_phone,'RowTimes',seconds(kps_phone{:,1}));
 ts_kinect = table2timetable(kps_kinect,'RowTimes',seconds(kps_kinect{:,1}));
 
+
+% ======== Change data input & timetable sync method here =========
+% 
 ts_cube = synchronize(ts_phone, ts_kinect,'Union','linear');
 
-%% Visualization
+%% Visualization (ts_cube)
 % =========== KPs & Data Selection ===========
-VData = 'kps_kinect';  %Data version
 KP1 = 'left_elbow';
 KP2 = 'left_wrist';
 % ============================================
@@ -71,10 +79,10 @@ KP2 = 'left_wrist';
 figure()
 title(strcat('X-axis time series data - ',KP1,' & ', KP2));
 hold on;
-plot(kps_phone.time,eval(strcat('kps_phone.',KP1,'_x')),'LineWidth',1.5,'Color','b','LineStyle','--');
-plot(kps_phone.time,eval(strcat('kps_phone.',KP2,'_x')),'LineWidth',1.5,'Color','b');
-plot(kps_kinect.time,eval(strcat(VData,'.',KP1,'_x')),'LineWidth',1.5,'Color','g','LineStyle','--');
-plot(kps_kinect.time,eval(strcat(VData,'.',KP2,'_x')),'LineWidth',1.5,'Color','g');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP1,'_x_ts_phone')),'LineWidth',1.5,'Color','b');   %,'LineStyle','--'
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP2,'_x_ts_phone')),'LineWidth',1.5,'Color','b');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP1,'_x_ts_kinect')),'LineWidth',1.5,'Color','g');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP2,'_x_ts_kinect')),'LineWidth',1.5,'Color','g');
 xlabel('Time (s)');
 ylabel('Pixel');
 legend({strcat('Mediapipe ',KP1), strcat('Mediapipe ',KP2), strcat('KinectV2 ', KP1), strcat('KinectV2 ', KP2)},'Location','Northeast')
@@ -82,15 +90,35 @@ legend({strcat('Mediapipe ',KP1), strcat('Mediapipe ',KP2), strcat('KinectV2 ', 
 figure()
 title(strcat('Y-axis time series data - ',KP1,' & ', KP2));
 hold on;
-plot(kps_phone.time,eval(strcat('kps_phone.',KP1,'_y')),'LineWidth',1.5,'Color','b','LineStyle','--');
-plot(kps_phone.time,eval(strcat('kps_phone.',KP2,'_y')),'LineWidth',1.5,'Color','b');
-plot(kps_kinect.time,eval(strcat(VData,'.',KP1,'_y')),'LineWidth',1.5,'Color','g','LineStyle','--');
-plot(kps_kinect.time,eval(strcat(VData,'.',KP2,'_y')),'LineWidth',1.5,'Color','g');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP1,'_y_ts_phone')),'LineWidth',1.5,'Color','b');   %,'LineStyle','--'
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP2,'_y_ts_phone')),'LineWidth',1.5,'Color','b');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP1,'_y_ts_kinect')),'LineWidth',1.5,'Color','g');
+plot(ts_cube.Time,eval(strcat('ts_cube.',KP2,'_y_ts_kinect')),'LineWidth',1.5,'Color','g');
 xlabel('Time (s)');
 ylabel('Pixel');
 legend({strcat('Mediapipe ',KP1), strcat('Mediapipe ',KP2), strcat('KinectV2 ', KP1), strcat('KinectV2 ', KP2)},'Location','Northeast')
 
-%% Save datacube
+%% Correlation Analysis
+
+% Breakdown data cube into correpsonding cubes
+ts_Kcube = ts_cube(:,29:54);
+ts_Pcube = ts_cube(:,2:27);
+
+% [R,P,RL,RU] = corrcoef(eval(strcat('ts_cube.',ts_cube.Properties.VariableNames{1})), eval(strcat('ts_cube.',ts_cube.Properties.VariableNames{2})));
+
+for i = 1:length(ts_Kcube.Properties.VariableNames)
+
+[R,P,RL,RU] = corrcoef(eval(strcat('ts_Kcube.',ts_Kcube.Properties.VariableNames{i})), eval(strcat('ts_Pcube.',ts_Pcube.Properties.VariableNames{i})));
+
+corrMat.Var(i) = ts_Kcube.Properties.VariableNames(i);
+corrMat.R(i) = R(2);
+corrMat.RU(i) = RU(2);
+corrMat.RL(i) = RL(2);
+corrMat.P(i) = P(2);
+end
+
+corrMat.Sig = corrMat.P <= 0.05;
 
 
-[R,P] = corrcoef(ts_cube.left_wrist_x_ts_kinect, ts_cube.left_wrist_x_ts_phone);
+%% RMSE Analysis
+
